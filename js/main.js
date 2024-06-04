f617943d2147f4a2075ac469e8152ec9f7fd8138
acute <awaisraobuddy@gmail.com>
2024-06-04 13:38:02 +0500

    Styling Completed

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// ========== CLOCK ==========
+const hour = document.getElementById("clock-hour"),
+minutes = document.getElementById("clock-minutes");
+
+// ========== Time and Data Text ==========
+const dateDayWeek = document.getElementById("date-day-week"),
+  dateMonth = document.getElementById("date-month"),
+  dateDay = document.getElementById("date-day"),
+  dateYear = document.getElementById("date-year"),
+  textHour = document.getElementById("text-hour"),
+  textMinutes = document.getElementById("text-minutes"),
+  textAmPm = document.getElementById("text-ampm");
